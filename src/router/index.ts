9e2904cc83a42bc2bd1c3b3a600b66cc78eee84e
EK <krakhalev.efim@gmail.com>
2024-08-28 21:11:07 -0400

    add inspirations route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import HistoryView from '../views/HistoryView.vue'
+import InspirationsView from '../views/InspirationsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,10 +14,12 @@ const router = createRouter({
     {
       path: '/history',
       name: 'history',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: HistoryView
+    },
+    {
+      path: '/inspirations',
+      name: 'inspirations',
+      component: InspirationsView
     }
   ]
 })
