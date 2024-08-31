b447f05f039077e7bb570103fe28237373c86424
EK <krakhalev.efim@gmail.com>
2024-08-30 22:53:37 -0400

    add prompt header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Lobby from '../components/Lobby.vue'
+import Prompt from '@/components/Prompt.vue';
 </script>
 
 <template>
   <main>
+    <Prompt input="whoami" />
     <img alt="logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
     <Lobby />
   </main>
