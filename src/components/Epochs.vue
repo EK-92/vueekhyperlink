3fae414556bcfebede070976dbd7107c74a404a3
EK <krakhalev.efim@gmail.com>
2024-08-30 22:32:49 -0400

    cleap up css, add info for history page

--- src/components/Epochs.vue
+++ src/components/Epochs.vue
@@ -1,40 +1,69 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import Epoch from './Epoch.vue'
+
+const cards = ref([
+  {
+    header: 'Graduated from Sheridan College',
+    subheader: 'Ontario College Diploma',
+    link: 'https://www.sheridancollege.ca/programs/computer-systems-technician-software-engineering',
+    date: '2016',
+    pic: '/src/assets/sheridan.png'
+  },
+  {
+    header: 'Internship @ The Silverlogic',
+    subheader: 'Front-end development',
+    link: 'https://tsl.io/',
+    date: '2016-2017',
+    pic: '/src/assets/tsl.svg'
+  },
+  {
+    header: 'Eligible to work in Canada',
+    date: '2016-current',
+    link: '#',
+    pic: '/src/assets/cic.png'
+  },
+  {
+    header: 'Employment @ Emeritus Financial',
+    subheader: 'Front-end development',
+    link: 'http://emeritusfinancial.com/',
+    date: '2017-2018',
+    pic: '/src/assets/emeritus.png'
+  },
+  {
+    header: 'Employment @ RewardOps',
+    subheader: 'Front-end development',
+    link: 'https://rewardops.com/',
+    date: '2018-2021',
+    pic: '/src/assets/rewardops.png'
+  },
+  {
+    header: 'Employment @ RewardOps',
+    subheader: 'SRE',
+    link: 'https://rewardops.com/',
+    date: '2021-2023',
+    pic: '/src/assets/rewardops.png'
+  },
+  {
+    header: 'Permanent Resident of Canada',
+    date: '2022-current',
+    link: '#',
+    pic: '/src/assets/cic.png'
+  },
+  {
+    header: '...',
+    subheader: '...',
+    link: '#',
+    date: '...',
+    pic: '/src/assets/cicShaded.png'
+  }
+])
 </script>
 
 <template>
-  <Epoch>
-    <template #heading>Sheridan</template>
-
-  </Epoch>
-
-  <Epoch>
-    <template #heading>TSL</template>
-
-  </Epoch>
-
-  <Epoch>
-    <template #heading>work permit</template>
-
-  </Epoch>
-
-  <Epoch>
-    <template #heading>Emeritus</template>
-  </Epoch>
-
-  <Epoch>
-    <template #heading>RO 1</template>
-  </Epoch>
-
-  <Epoch>
-    <template #heading>RO 2</template>
-  </Epoch>
-
-  <Epoch>
-    <template #heading>PR</template>
-  </Epoch>
-
-  <Epoch>
-    <template #heading>Future</template>
-  </Epoch>
+  <div class="cards-holder">
+    <Epoch v-for="card in cards" :content="card"> </Epoch>
+  </div>
 </template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,24 +5,8 @@ import Lobby from '../components/Lobby.vue'
 <template>
   <main>
     <img alt="logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
-
-    <Lobby/>
+    <Lobby />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-@media (min-width: 1024px) {
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
+<style scoped></style>
