55107d99a176cfa96dc4295402ab3cede15fbcf8
EK <krakhalev.efim@gmail.com>
2024-08-30 22:58:57 -0400

    no inline hardcoding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@ import Prompt from '@/components/Prompt.vue';
 <template>
   <main>
     <Prompt input="whoami" />
-    <img alt="logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+    <img alt="logo" class="logo" src="@/assets/logo.png" />
     <Lobby />
   </main>
 </template>
