07b7d1813ebd77484c5284eec0582e4dcda580ea
EK <krakhalev.efim@gmail.com>
2024-09-11 20:17:43 -0400

    fix image paths (#13)

--- src/components/Epochs.vue
+++ src/components/Epochs.vue
@@ -17,54 +17,54 @@ const cards = ref([
     subheader: 'Ontario College Diploma',
     link: 'https://www.sheridancollege.ca/programs/computer-systems-technician-software-engineering',
     date: '2016',
-    pic: '/src/assets/sheridan.png'
+    pic: '@/assets/sheridan.png'
   },
   {
     header: 'Internship @ The Silverlogic',
     subheader: 'Front-end development',
     link: 'https://tsl.io/',
     date: '2016-2017',
-    pic: '/src/assets/tsl.svg'
+    pic: '@/assets/tsl.svg'
   },
   {
     header: 'Eligible to work in Canada',
     date: '2016-current',
     link: '',
-    pic: '/src/assets/cic.png'
+    pic: '@/assets/cic.png'
   },
   {
     header: 'Employment @ Emeritus Financial',
     subheader: 'Front-end development',
     link: 'http://emeritusfinancial.com/',
     date: '2017-2018',
-    pic: '/src/assets/emeritus.png'
+    pic: '@/assets/emeritus.png'
   },
   {
     header: 'Employment @ RewardOps',
     subheader: 'Front-end development',
     link: 'https://rewardops.com/',
     date: '2018-2021',
-    pic: '/src/assets/rewardops.png'
+    pic: '@/assets/rewardops.png'
   },
   {
     header: 'Employment @ RewardOps',
     subheader: 'SRE',
     link: 'https://rewardops.com/',
     date: '2021-2023',
-    pic: '/src/assets/rewardops.png'
+    pic: '@/assets/rewardops.png'
   },
   {
     header: 'Permanent Resident of Canada',
     date: '2022-current',
     link: '',
-    pic: '/src/assets/cic.png'
+    pic: '@/assets/cic.png'
   },
   {
     header: '...',
     subheader: '...',
     link: '',
     date: '...',
-    pic: '/src/assets/cicShaded.png'
+    pic: '@/assets/cicShaded.png'
   }
 ])
 </script>
