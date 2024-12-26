37a1fd94b98b8a1aca221099f8983dd134f1e8d6
EK <krakhalev.efim@gmail.com>
2024-12-26 17:55:40 -0500

    add chartjs (#17)
    * add chartjs, npm audit
    
    * add chart, replace profile pic, replace logo, replace favicon
    
    * modify menu, set up graph canvas size
    
    * cleanup
    
    * remove import
    
    * any fix
    
    * fix mobile icon placing, align menu list
    
    * css cleanup, restore graph tooltip
    
    * adjust mobile media queries
    
    * adjust spacing, switch to viewpoint units, simplify calc logic
    
    * fix history page min width, adjust shadows radius

---
+++ src/components/Chartacter.vue
@@ -0,0 +1,56 @@
+  <script lang="ts">
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+  } from 'chart.js'
+  import { Bar } from 'vue-chartjs'
+
+  ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+  const options: any = {
+    responsive: true,
+    indexAxis: 'y',
+    scales: {
+      y: {
+        ticks: { color: '#7cb342', beginAtZero: true }
+      },
+      x: {
+        ticks: { color: '#7cb342', beginAtZero: true }
+      }
+    },
+    plugins: {
+      legend: {
+        display: false
+      },
+    }
+
+  }
+
+  export default {
+    name: 'Chartacter',
+    components: {
+      Bar
+    },
+    data() {
+      return {
+        data: {
+          labels: ['Luck', 'Intuition', 'Intellect', 'Persistence', 'Energy', 'Resourcefulness', 'Ambition', 'Patience'],
+          datasets: [{
+            data: [8, 7, 6, 9, 8, 10, 5, 8],
+            backgroundColor: '#7cb342'
+          }]
+        },
+        options
+      }
+    }
+  }
+</script>
+
+<template>
+  <Bar :data="data" :options="options" />
+</template>
