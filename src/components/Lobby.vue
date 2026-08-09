3da2b1093aed905427c2b028b3c78d326967b862
EK <krakhalev.efim@gmail.com>
2026-08-09 19:45:05 -0400

    typo

--- src/components/Lobby.vue
+++ src/components/Lobby.vue
@@ -21,7 +21,7 @@ const themeClass = computed(() => ({
     <div class="name">
       <img alt="logo" :src="logo" />
       <h1>Efim Krakhalev (EK)</h1>
-      <h3>Front-end Developer & SRE</h3>
+      <h3>Full-stack Developer & SRE</h3>
       <h3>Location: Toronto, ON</h3>
       <Chartacter />
     </div>
